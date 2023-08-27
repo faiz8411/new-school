c61f597a8c50b6f1aff29cd19351d604ef235339
faiz8411 <faizullah.noa2014@gmail.com>
2023-08-27 11:28:35 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import config from "./config";
+import app from "./app";
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database as string);
+    console.log("successfully connected  ");
+    app.listen(config.port, () => {
+      console.log(`school app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log("failed", err);
+  }
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+bootstrap();
